15ceceef458baa557f445a958cf457ccaa2a1835
wasee-sun <revengersun@gmail.com>
2025-04-18 17:44:08 +0600

    Linux command functionality divided
    Co-authored-by: sahil-iftekhar <sahil-iftekhar@users.noreply.github.com>

--- input.c
+++ input.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define MAX_INPUT 1024
+#define MAX_TOKENS 64
 
 int shell_input(char *input) {
     if (fgets(input, MAX_INPUT, stdin) == NULL) {
@@ -12,7 +13,52 @@ int shell_input(char *input) {
 
     input[strcspn(input, "\n")] = '\0';
 
-    printf("Command: %s\n", input);
+    if (strlen(input) == 0) {
+        printf("Command: none, Option: none, Argument: none\n");
+        return 0;
+    }
+
+    char *tokens[MAX_TOKENS];
+    int count_token = 0;
+    char *token = strtok(input, " \t");
+    
+    while (token != NULL) {
+        tokens[count_token] = token;
+        count_token++;
+        token = strtok(NULL, " \t");
+    }
+
+    char *command = NULL;
+    char *option = NULL;
+    char *argument = NULL;
+
+    if (count_token > 0) {
+        command = tokens[0];
+    
+        int i = 1;
+        while (i < count_token) {
+            if (tokens[i][0] == '-') {
+                option = tokens[i];
+            } else {
+                argument = malloc(1024);
+                if (!argument) {
+                    perror("Error allocating memory");
+                    exit(1);
+                }
+                strcpy(argument, tokens[i]);
+                for (int j = i + 1; j < count_token; j++) {
+                    strcat(argument, " ");
+                    strcat(argument, tokens[j]);
+                }
+                break;
+            }
+        }
+    }
+
+    printf("Command: %s, Option: %s, Argument: %s\n",
+        command ? command : "none",
+        option ? option : "none",
+        argument ? argument : "none");
 
     if (strcmp(input, "exit") == 0) {
         return 1;
